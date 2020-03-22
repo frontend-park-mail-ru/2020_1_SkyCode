bced6aa13186b6009e258e6228ab67c5da2e4c90
Arthur <aptyp.potatoch@gmail.com>
2020-03-22 12:09:24 +0300

    base and main page added

---
+++ public/pages/BasePage.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class BasePage {
+    _title;
+    _view;
+
+    constructor({title}) {
+        this._title = title;
+    }
+
+    // У вьюхи должны быть методы bind, unbind, HTML и геттер state
+    show(view) {
+        this._view = view;
+        document.body.innerHTML = this._view.HTML();
+        this._view.bind();
+    }
+
+    hide() {
+        this._view.unbind();
+        this._view = void 0;
+    }
+
+    get state() {
+        if (this._view === undefined){
+            this.log('an attempt to get undef state');
+            return;
+        }
+
+        return this._view.state;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    log(message) {
+        let start = '\n' + this.constructor.name + ':\t';
+        console.log(start + message);
+    }
+}
+
+export default BasePage;
