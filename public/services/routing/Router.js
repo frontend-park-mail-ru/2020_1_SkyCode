e83bc13f9ce63dfe8e87bd24266aed6509d13da5
Arthur <aptyp.potatoch@gmail.com>
2020-03-22 11:42:59 +0300

    routing added

---
+++ public/services/routing/Router.js
@@ -0,0 +1,74 @@
+'use strct';
+
+import EventBus from '../EventBus.js';
+
+class Router {
+    _currentPage;
+
+    constructor() {
+        this._pages = [];
+        this._registerAllPages();
+        EventBus.listen('set-page', (this._goto).bind(this));
+        window.onpopstate = (this._histMove).bind(this);
+    }
+
+    _histMove(event) {
+        // Невозможно сохранить актуальное состояние прошлой страницы, т.к. запись прошлой истории недоступна
+        // Мы даже не знаем, было ли нажато hist.front() или hist.back(), чтобы при помощи обратных команд
+        // выйти на нужные записи, изменить их и вернуться на текущую
+        event.preventDefault();
+        this._currentPage.hide();
+        this._currentPage = this._retreivePage(document.location) || log('unknown url') || Page404;
+        this._currentPage.show(document.location, event.state);
+    }
+
+    _goto({url}) {
+        let state = {};
+
+        if (this._currentPage) {
+            state = this._currentPage.state;
+            this._savePageStateInHistory(this._currentPage);
+            this._currentPage.hide();
+        }
+
+        this._currentPage = this._retreivePage(url) || log('unknown url') || Page404;
+        this._setNewHistoryRecord(this._currentPage, url);
+        this._currentPage.show(url, state);
+    }
+
+    _savePageStateInHistory(page) {
+        history.replaceState(page.state, page.title); // url the same
+    }
+
+    _setNewHistoryRecord(page, url) {
+        history.pushState(page.state, page.title, url);
+    }
+
+    _registerAllPages() {
+        this._registerPage(MainPage, url);
+    }
+
+    _registerPage(page, url) {
+        this._pages.push({
+            // будут использоваться только целочисленные части url?
+            pattern: new RegExp('^' + url.replace(/:\w+/, '(d+)') + '$'),
+            page: page,
+        });
+    }
+
+    _retreivePage(url) {
+        for (let pg of this._pages) {
+            if (url.match(pg.pattern)) {
+                return pg.page;
+            }
+        }
+    }
+}
+
+let log = function (message) {
+    let start = '\nRouter:\t';
+    console.log(start + message);
+};
+
+
+export default new Router();
