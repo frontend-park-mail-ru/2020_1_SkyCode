87bb30eab88dd4dfc0a5544040050be055808a64
Arthur <aptyp.potatoch@gmail.com>
2020-03-28 18:22:21 +0300

    basket added

--- public/render/blocks/order/Order.js
+++ public/render/blocks/order/Order.js
@@ -1,6 +1,7 @@
 import Component from '../../Component.js';
 import ImageHref from '../imageHref/ImageHref.js';
 import PlaceTimeCard from '../PlaceTimeCard/PlaceTimeCard.js';
+import Basket from '../basket/Basket.js';
 
 export default class Order extends Component {
     constructor({profile = {src: '/static/profile.png', href: 'login'},
@@ -17,6 +18,25 @@ export default class Order extends Component {
             title: 'My Order',
             placeTimeCard: new PlaceTimeCard({
                 classes: 'order__place-time-card',
+            }),
+            basket: new Basket({
+                classes: 'order__basket',
+                products: [{
+                    imageHref: 'static/basket/BBQBurger.jpg',
+                    quantity: 1,
+                    name: 'BBQ Burger',
+                    cost: 339,
+                }, {
+                    imageHref: 'static/basket/FrenchFries.jpg',
+                    quantity: 2,
+                    name: 'French fries',
+                    cost: 139,
+                }, {
+                    imageHref: 'static/basket/CocaCola.jpg',
+                    quantity: 1,
+                    name: 'Coca-cola',
+                    cost: 99,
+                }],
             }),
         });
     }
