cab2e3690dbba9439ac9a4087d4282d3910bcc64
Arthur <aptyp.potatoch@gmail.com>
2020-03-31 16:09:22 +0300

    Header added

--- public/render/views/LoginSignupView/LoginSignupView.js
+++ public/render/views/LoginSignupView/LoginSignupView.js
@@ -1,12 +1,16 @@
 import Component from '../../Component.js';
 import LoginField from '../../blocks/loginField/LoginField.js';
 import SignupField from '../../blocks/signupField/SignupField.js';
+import Header from '../../blocks/header/Header.js';
 
 
 export default class LoginSignupView extends Component {
     constructor() {
         super('log');
         this.addContextData({
+            Header: new Header({
+                classes: 'header',
+            }),
             LoginField: new LoginField({
                 classes: 'login-signup__login',
             }),
