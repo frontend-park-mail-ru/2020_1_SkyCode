ba52a63e10db1de07e4c1775ecac91b25a78378b
Arthur <aptyp.potatoch@gmail.com>
2020-04-05 17:21:02 +0300

    add product page validation added

--- public/controllers/AddProductByRestaurantController.js
+++ public/controllers/AddProductByRestaurantController.js
@@ -4,39 +4,49 @@ import EventBus from '../services/Events/EventBus.js';
 import RestaurantModel from '../models/RestaurantModel.js';
 
 class AddProductByRestaurantController extends BaseController {
-	constructor(title = 'Add product') {
-		super(title);
-	}
+    constructor(title = 'Add product') {
+        super(title);
+    }
 
-	run() {
-		super.run(new AddProductByRestaurantView());
-	}
+    run() {
+        super.run(new AddProductByRestaurantView());
+    }
 
-	startCatchEvents() {
-		EventBus.subscribe('add-product-by-restaurant', this.addProductCb.bind(this));
-		EventBus.subscribe('add-product-img-restaurant', this.addProductImgCb.bind(this));
-	}
+    startCatchEvents() {
+        EventBus.subscribe('add-product-by-restaurant', this.addProductCb.bind(this));
+        EventBus.subscribe('add-product-img-restaurant', this.addProductImgCb.bind(this));
+    }
 
-	stopCatchEvents() {
-		EventBus.unsubscribe('add-product-by-restaurant', this.addProductCb.bind(this));
-		EventBus.unsubscribe('add-product-img-restaurant', this.addProductImgCb.bind(this));
-	}
+    stopCatchEvents() {
+        EventBus.unsubscribe('add-product-by-restaurant', this.addProductCb.bind(this));
+        EventBus.unsubscribe('add-product-img-restaurant', this.addProductImgCb.bind(this));
+    }
 
-	addProductCb(data) {
-		RestaurantModel.addProduct(1, data).then(response => {
-			if (response.message) {
-				EventBus.publish('set-page', {url: '/restaurants'});
-			}
-		}).catch(err => console.log(err));
-	}
+    addProductCb(data) {
+        RestaurantModel
+            .addProduct(1, data)
+            .then(response => {
+            	if (response.error) {
+            		EventBus.publish('add-product-error', response.error);
+				} else if (response.message) {
+                    EventBus.publish('set-page', {url: '/restaurants'});
+                }
+            })
+            .catch(err => {
+            	EventBus.publish('add-product-error', err);
+            	console.log(err);
+			});
+    }
 
-	addProductImgCb(data) {
-		RestaurantModel.addProductImage(1, data).then(response => {
-			if (response.message) {
-				EventBus.publish('set-page', {url: '/add'});
-			}
-		}).catch(err => console.log(err));
-	}
+    addProductImgCb(data) {
+        RestaurantModel.addProductImage(1, data).then(response => {
+			if (response.error) {
+				EventBus.publish('add-product-image-error', response.error);
+			} else if (response.message) {
+                EventBus.publish('set-page', {url: '/add'});
+            }
+        }).catch(err => console.log(err));
+    }
 }
 
 export default new AddProductByRestaurantController();
