fe036106f7546ae65d151492b51c1dc9e242fd34
Arthur <aptyp.potatoch@gmail.com>
2020-05-03 14:00:42 +0300

    resizable restaurant view done

--- public/render/views/RestaurantView/RestaurantView.js
+++ public/render/views/RestaurantView/RestaurantView.js
@@ -1,22 +1,48 @@
 import Component from '../../Component.js';
 import BaseView from '../BaseView/BaseView';
-import RestaurantViewMainArea
-    from '../../blocks/restaurantViewMainArea/RestaurantViewMainArea';
 import temp from './RestaurantView.hbs';
+import IconedHeader from '../../blocks/iconedHeader/IconedHeader';
+import WavingMenue from '../../blocks/wavingMenue/WavingMenue';
+import RestaurantBanner from '../../blocks/restaurantBanner/restaurantBanner';
+import RestaurantCategories
+    from '../../blocks/restaurantCategories/RestaurantCategories';
+import ProductList from '../../blocks/products/ProductList';
+import Href from '../../elements/href/Href';
 
-class RestaurantView extends Component {
+export default class RestaurantView extends BaseView {
     constructor({restaurant, products, categoryArr}) {
-        super('', {
-            BaseView: new BaseView({
-                MainArea: new RestaurantViewMainArea({
-                    restaurant,
-                    products,
-                    categoryArr,
-                }),
+        super({
+            Main: new MainArea({
+                restaurant,
+                products,
+                categoryArr,
             }),
+            Header: new IconedHeader({classes: 'base-view__header'}),
+            LeftBar: new WavingMenue(),
         });
-        this.template = temp;
     }
 }
 
-export default RestaurantView;
+class MainArea extends Component {
+    constructor({restaurant, products, categoryArr}) {
+        super();
+        super.template = temp;
+        this.addContextData({
+            Banner: new RestaurantBanner({
+                classes: 'restaurant-banner',
+                imgHref: `/images/${restaurant.image}`,
+                rate: restaurant.rating,
+                name: restaurant.name,
+            }),
+            Categories: new RestaurantCategories({categoryArr}),
+            Products: new ProductList({
+                classes: 'restaurant-view__product-list',
+                productArr: products}),
+            InfoHref: new Href({
+                classes: 'restaurant-view__info',
+                text: 'О нас / отзывы',
+                href: `/restaurants/${restaurant.id}/info`,
+            }),
+        });
+    }
+}
