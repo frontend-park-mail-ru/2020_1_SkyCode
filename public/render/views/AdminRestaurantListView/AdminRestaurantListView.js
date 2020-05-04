9901be5b9dca1136245d1f759ac81869b91b2a7c
Arthur <aptyp.potatoch@gmail.com>
2020-05-04 19:06:35 +0300

    restaurant list view mobile done

--- public/render/views/AdminRestaurantListView/AdminRestaurantListView.js
+++ public/render/views/AdminRestaurantListView/AdminRestaurantListView.js
@@ -2,14 +2,28 @@ import Component from '../../Component.js';
 import Header from '../../blocks/header/Header.js';
 import template from './AdminRestaurantListView.hbs';
 import RestaurantsList from '../../blocks/restaurantsList/restaurantsList.js';
+import BaseView from '../BaseView/BaseView';
+import IconedHeader from '../../blocks/iconedHeader/IconedHeader';
+import WavingMenue from '../../blocks/wavingMenue/WavingMenue';
+import Order from '../../blocks/order/Order';
 
-export default class AdminRestaurantsListView extends Component {
+export default class AdminRestaurantsListView extends BaseView {
+    constructor({restaurantsArray}) {
+        super({
+            Main: new MainArea({restaurantsArray}),
+            Header: new IconedHeader({classes: 'base-view__header'}),
+            LeftBar: new WavingMenue(),
+            AddOnes: [
+                new Order(),
+            ],
+        });
+    }
+}
+
+class MainArea extends Component {
     constructor({restaurantsArray}) {
         super();
         this.addContextData({
-            Header: new Header({
-                classes: 'header',
-            }),
             RestaurantList: new RestaurantsList({
                 classes: 'restaurants-list',
                 restaurantsArr: restaurantsArray,
