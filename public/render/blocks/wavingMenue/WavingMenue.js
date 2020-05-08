88c2b885689b840abb26a9575e06d88bb3110184
Arthur <aptyp.potatoch@gmail.com>
2020-05-08 17:17:09 +0300

    simple waving list added

--- public/render/blocks/wavingMenue/WavingMenue.js
+++ public/render/blocks/wavingMenue/WavingMenue.js
@@ -1,17 +1,44 @@
 import Component from '../../Component.js';
 import EventBus from '../../../services/Events/EventBus.js';
 import template from './WavingMenue.hbs';
+import Href from '../../elements/href/Href';
 
 export default class WavingMenue extends Component {
     constructor({classes} = {}) {
         classes = classes || 'waving-menue';
         super(classes);
+        const userHrefs = [];
+
+        userHrefs.push(new Href({
+            classes: 'waving-menue__href',
+            text: 'Профиль',
+            href: '/profile',
+        }));
+
+        userHrefs.push(new Href({
+            classes: 'waving-menue__href',
+            text: 'Поддержка',
+            href: '/support',
+        }));
+
+        userHrefs.push(new Href({
+            classes: 'waving-menue__href',
+            text: 'Карта',
+            href: '/map',
+        }));
+
+        userHrefs.push(new Href({
+            classes: 'waving-menue__href',
+            text: 'История заказов',
+            href: '/orders',
+        }));
+        this.addContextData({userHrefs});
+
         this.isVisible = false;
         super.template = template;
     }
 
     bind() {
-        this.appear();
         EventBus.subscribe('hamburger-button-clicked', () => {
             if (this.isVisible) {
                 this.disappear();
@@ -20,12 +47,15 @@ export default class WavingMenue extends Component {
             }
         });
 
+        document.body.getElementsByClassName('waving-menue__hider')[0]
+            .onclick = (e) => {
+                e.preventDefault();
+            };
+
         super.bind();
-        this.disappear();
     }
 
     unbind() {
-        this.appear();
         EventBus.unsubscribe('hamburger-button-clicked', () => {
             if (this.isVisible) {
                 this.disappear();
@@ -34,21 +64,40 @@ export default class WavingMenue extends Component {
             }
         });
 
-        super.bind();
-        this.disappear();
+        document.body.getElementsByClassName('waving-menue__hider')[0]
+            .onclick = null;
+
+
+        super.unbind();
     }
 
     appear() {
         if (!this.isVisible) {
-            this.domElement.style.left = '0';
-            this.isVisible = true;
+            document.body.getElementsByClassName('waving-menue__hider')[0]
+                .style.display = 'block';
+            this.domElement.style.display = 'block';
+
+            setTimeout(() => {
+                document.body.getElementsByClassName('waving-menue__hider')[0]
+                    .style.opacity = '1';
+                this.domElement.style.opacity = '1';
+                this.isVisible = true;
+            }, 100);
         }
     }
 
     disappear() {
         if (this.isVisible) {
-            this.domElement.style.left = '-5000px';
-            this.isVisible = false;
+            document.body.getElementsByClassName('waving-menue__hider')[0]
+                .style.opacity = '0';
+            this.domElement.style.opacity = '0';
+
+            setTimeout(() => {
+                document.body.getElementsByClassName('waving-menue__hider')[0]
+                    .style.display = 'none';
+                this.domElement.style.display = 'none';
+                this.isVisible = false;
+            }, 500);
         }
     }
 }
