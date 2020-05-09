9d31f6eca5b9ebc1c57c35c20dbc8d2dd2a930df
Arthur <aptyp.potatoch@gmail.com>
2020-05-09 09:21:35 +0300

    token system improved

--- public/models/Http.js
+++ public/models/Http.js
@@ -20,63 +20,56 @@ class Http {
             req.body = body;
         }
         if (headers === true) {
+            req.headers = {};
             if (type === 'json') {
-                req.headers = {
+                Object.assign(req.headers, {
                     'Content-Type': 'application/json',
+                });
+            }
+            if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
+                Object.assign(req.headers, {
                     'X-Csrf-Token': localStorage.getItem('token'),
-                };
-            } else if (type === 'file') {
-                req.headers = {
-                    'X-Csrf-Token': localStorage.getItem('token'),
-                };
+                });
             }
         }
-        return fetch(`${this.serverPath}${path}`, req);
+        return fetch(`${this.serverPath}${path}`, req)
+            .then((response) => Http.retCSRFToken(response));
     }
 
     async fetchGet({path}) {
-        const response = await this.fetchRequest({method: 'GET',
+        return await this.fetchRequest({
+            method: 'GET',
             path,
-            m: 'cors',
         });
-
-        Http.retreiveCSRFToken(response);
-        return response;
     }
 
     async fetchPost({path, body, type = 'json'}) {
-        const response = await this.fetchRequest({method: 'POST',
+        return await this.fetchRequest({
+            method: 'POST',
             path,
             body,
             type,
         });
-
-        Http.retreiveCSRFToken(response);
-        return response;
     }
 
     async fetchPut({path, body, type = 'json'}) {
-        const response = await this.fetchRequest({method: 'PUT',
+        return await this.fetchRequest({
+            method: 'PUT',
             path,
             body,
             type,
         });
-
-        Http.retreiveCSRFToken(response);
-        return response;
     }
 
     async fetchDelete({path, body}) {
-        const response = await this.fetchRequest({method: 'DELETE',
+        return await this.fetchRequest({
+            method: 'DELETE',
             path,
             body,
         });
-
-        Http.retreiveCSRFToken(response);
-        return response;
     }
 
-    static retreiveCSRFToken(response) {
+    static retCSRFToken(response) {
         const token = response.headers.get('X-Csrf-Token');
         if (token) {
             localStorage.setItem('token', token);
