ab70908a9d29f5b1869dc40eb5a3bf84b45460dc
Arthur <aptyp.potatoch@gmail.com>
2020-05-22 14:31:35 +0300

    order hist view done

--- public/render/views/OrderHistoryView/OrderHistoryView.js
+++ public/render/views/OrderHistoryView/OrderHistoryView.js
@@ -1,25 +1,9 @@
 import Component from '../../Component.js';
 import template from './OrderHistoryView.hbs';
 import OrderHistory from '../../blocks/orderHistory/orderHistory.js';
-import BaseView from '../BaseView/BaseView';
-import IconedHeader from '../../blocks/iconedHeader/IconedHeader';
-import WavingMenue from '../../blocks/wavingMenue/WavingMenue';
-import Order from '../../blocks/order/Order';
 
-export default class OrderHistoryView extends BaseView {
-    constructor(orders) {
-        super({
-            Main: new MainArea(orders),
-            Header: new IconedHeader({classes: 'base-view__header'}),
-            LeftBar: new WavingMenue(),
-            AddOnes: [
-                new Order(),
-            ],
-        });
-    }
-}
 
-class MainArea extends Component {
+export default class OrderHistoryView extends Component {
     constructor(orders) {
         const message = sessionStorage.message;
         sessionStorage.message = '';
