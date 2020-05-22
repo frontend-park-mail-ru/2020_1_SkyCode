51c96a8d24ba54f13eeb5fb6f20b91214726188b
Arthur <aptyp.potatoch@gmail.com>
2020-05-22 14:45:10 +0300

    maps done

--- public/render/views/MapView/MapView.js
+++ public/render/views/MapView/MapView.js
@@ -1,25 +1,8 @@
 import Component from '../../Component.js';
-import Order from '../../blocks/order/Order.js';
 import template from './MapView.hbs';
-import BaseView from '../BaseView/BaseView';
-import IconedHeader from '../../blocks/iconedHeader/IconedHeader';
-import WavingMenue from '../../blocks/wavingMenue/WavingMenue';
 
 
-export default class MapView extends BaseView {
-    constructor() {
-        super({
-            Main: new MainArea(),
-            Header: new IconedHeader({classes: 'base-view__header'}),
-            LeftBar: new WavingMenue(),
-            AddOnes: [
-                new Order(),
-            ],
-        });
-    }
-}
-
-class MainArea extends Component {
+export default class MapView extends Component {
     constructor() {
         super();
         super.template = template;
