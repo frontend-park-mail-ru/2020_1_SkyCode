4333346a924ad61c7f6abf427ce9669a6976c4c5
Arthur <aptyp.potatoch@gmail.com>
2020-05-22 13:36:52 +0300

    checkout view done

--- public/render/views/CheckoutView/CheckoutView.js
+++ public/render/views/CheckoutView/CheckoutView.js
@@ -1,35 +1,8 @@
 import Component from '../../Component.js';
-import Order from '../../blocks/order/Order.js';
 import OrderCheckout from '../../blocks/orderCheckout/OrderCheckout.js';
 import template from './CheckoutView.hbs';
-import BaseView from '../BaseView/BaseView';
-import IconedHeader from '../../blocks/iconedHeader/IconedHeader';
-import WavingMenue from '../../blocks/wavingMenue/WavingMenue';
-import EventBus from '../../../services/Events/EventBus';
 
-export default class CheckoutView extends BaseView {
-    constructor({profile, personNum}) {
-        super({
-            Main: new MainArea({
-                profile,
-                personNum,
-            }),
-            Header: new IconedHeader({classes: 'base-view__header'}),
-            LeftBar: new WavingMenue(),
-            AddOnes: [
-                new Order({
-                    withCheckoutButton: false,
-                }),
-            ],
-        });
-    }
-
-    bind() {
-        super.bind();
-    }
-}
-
-class MainArea extends Component {
+export default class CheckoutView extends Component {
     constructor({profile, personNum}) {
         super();
         super.template = template;
